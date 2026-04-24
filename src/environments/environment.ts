4eb764b217d0ff67bfea002009c9753b055d6328
decade <20120220@student.hcmus.edu.vn>
2026-04-25 00:44:27 +0700

    chore: gg client id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+    production: false,
+    apiUrl: 'http://localhost:8080',
+    googleClientId: '750825503616-ljl54thpbt448cp1sk8o4p6ogs9humlk.apps.googleusercontent.com',
 };
 
